e7aeb25ea67fedb2179bdf0af6561d26a8adebe0
erick <erickcarras1cgmail.com>
2026-08-31 18:28:52 -0500

    creacion y codificacion de inicio.php

---
+++ vista/inicio.php
@@ -0,0 +1,13 @@
+<?php include 'views/partials/header.php'; ?>
+<?php include 'views/partials/nav.php'; ?>
+
+<div class="container mt-4">
+    <div class="p-5 bg-light rounded-3">
+        <h1 class="display-4">Bienvenido al Sistema Bancario</h1>
+        <p class="lead">Accede a las funciones desde el menú o mediante la URL.</p>
+        <hr class="my-4">
+        <p>Prueba: <code>?accion=login&u=Noe&p=1234</code></p>
+    </div>
+</div>
+
+<?php include 'views/partials/footer.php'; ?>
