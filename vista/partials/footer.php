0cb142092547620e29a9c53db7da112bc8db054c
erick <erickcarras1cgmail.com>
2026-08-31 18:24:15 -0500

    codificacion de footer.php

---
+++ vista/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-light text-center text-lg-start mt-4">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2025 Banco ISTO - Todos los derechos reservados
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
