55d46f1a940cda7777fe1f332f6e3bd7914b2292
erick <erickcarras1cgmail.com>
2026-08-31 18:03:53 -0500

    modificacion de conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,11 +1,10 @@
 <?php
-class Database {
-    public static function conectar() {
-        $conexion = new mysqli("localhost", "root", "", "banco_erick");
+class conexion {
+    public static function conectar(){
+        $conexion = new mysqli("localhost","","banco_carrasco");
         if ($conexion->connect_error) {
-            die("Error de conexión: " . $conexion->connect_error);
+            die("Error de conexion: ". $conexion->connect_error);
         }
         return $conexion;
     }
 }
-?>
