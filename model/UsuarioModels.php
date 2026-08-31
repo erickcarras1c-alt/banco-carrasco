01b17507daf4437818bc95443a8b1798263add0b
erick <erickcarras1cgmail.com>
2026-08-31 18:41:42 -0500

    modificacion del codigo en model/UsuarioModels.php

--- model/UsuarioModels.php
+++ model/UsuarioModels.php
@@ -3,17 +3,28 @@ class UsuarioModel {
     private $db;
 
     public function __construct() {
-        $this->db = Database::conectar();
+        $this->db = conexion::conectar();
     }
 
-    public function verificarLogin($usuario, $password) {
-        $sql = "SELECT * FROM usuarios WHERE nombre_usuario = '$usuario' AND contraseña = '$password'";
+    public function verificarLogin($nombre_usuario, $contraseña) {
+        $sql = "SELECT * FROM usuarios WHERE nombre_usuario = '$nombre_usuario' AND contraseña = '$contraseña'";
         $resultado = $this->db->query($sql);
         return $resultado->fetch_assoc();
     }
+
     public function actualizarSaldo($id, $nuevoSaldo) {
         $sql = "UPDATE usuarios SET saldo = $nuevoSaldo WHERE id_usuario = $id";
         return $this->db->query($sql);
+    }
+
+    public function listarUsuarios() {
+        $sql = "SELECT id_usuario, nombre_usuario, saldo FROM usuarios";
+        $resultado = $this->db->query($sql);
+        $usuarios = [];
+        while ($fila = $resultado->fetch_assoc()) {
+            $usuarios[] = $fila;
+        }
+        return $usuarios;
     }
 }
 ?>
